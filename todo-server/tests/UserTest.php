0798255859b294e48ac37a350bae5d3ea9063774
Daniel Leussa <leussadaniel04@gmail.com>
2023-03-21 11:51:32 +0100

    Integrated unit tests for User class

---
+++ todo-server/tests/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Todolist\Model\User;
+
+final class UserTest extends TestCase 
+{
+    public function testClassConstructor()
+    {
+        $user = new User(1, 'John', 'Doe', 'johndoe@localhost', 'password', null);
+        $this->assertSame(1, $user->id, "Checking if the id is same");
+        $this->assertSame('John', $user->firstname, "Checking if the id is same");
+        $this->assertSame('johndoe@localhost', $user->email, "Checking if the id is same");
+    }
+}
