a6fd7f960cd01d17afc289fd3af605bc0d30228e
Daniel Leussa <leussadaniel04@gmail.com>
2023-03-21 22:09:26 +0100

    static method Router::error404 created, just to get my default error 404 page

--- todo-server/src/public/index.php
+++ todo-server/src/public/index.php
@@ -13,5 +13,17 @@ Utils::debug($_GET);
 
 //////////////////
 $target = DEFAULT_TARGET_URL;
+if(isset($_GET) && !empty($_GET['target']))
+{
+    $tmp_target = $_GET['target'];
+    if(in_array('/'.$tmp_target, Router::routesName()))
+    {
+
+    } else {
+        // show the error document
+        // header('Content-Type: application/json');
+        echo json_encode(Router::error404($_SERVER['REQUEST_URI']), JSON_UNESCAPED_SLASHES);
+    }
+}
 
 Utils::debug(Router::routesName());
